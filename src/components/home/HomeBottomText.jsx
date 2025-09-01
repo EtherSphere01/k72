34e0cc019de2489ecd6f5e2aa835b046adcc0ffd
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-09-01 22:56:01 +0600

    Home page

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const HomeBottomText = () => {
     return (
-        <div className="font-[font1] flex items-center justify-center gap-2 ">
+        <div className="font-[font2] flex items-center justify-center gap-2 ">
             <div className="border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] leading-[4.5vw] flex items-center justify-center px-10 border-white rounded-full uppercase">
                 <Link className="text-[6vw] mt-6" to="/projects">
                     Projects
@@ -12,7 +12,7 @@ const HomeBottomText = () => {
 
             <div className="border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] leading-[4.5vw] flex items-center justify-center px-10 border-white rounded-full uppercase">
                 <Link className="text-[6vw] mt-6" to="/agence">
-                    Agence
+                    Agency
                 </Link>
             </div>
         </div>
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,14 +1,65 @@
-import React from 'react'
+import React from "react";
 
 const Agence = () => {
-  return (
-      <div>
-          <div>
-              h1
-              
-          </div>
-    </div>
-  )
-}
+    return (
+        <>
+            <div className="section1">
+                <div className="h-[20vw] w-[15vw] absolute top-72 left-[30vw] rounded-[20px] overflow-hidden">
+                    <img
+                        className="h-full w-full object-cover"
+                        src="./images/agency1.jpg"
+                    ></img>
+                </div>
+                <div className="font-[font2] relative">
+                    <div className="mt-[55vh]">
+                        <h1 className="text-[19vw] uppercase leading-[17vw] text-center">
+                            SEVEN7Y <br></br> TWO
+                        </h1>
+                    </div>
+                    <div className="pl-[40%] font-[font1]">
+                        <p className="text-6xl ">
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            We’re inquisitive and open-minded, and we make sure
+                            creativity crowds out ego from every corner. A brand
+                            is a living thing, with values, a personality and a
+                            story. If we ignore that, we can achieve short-term
+                            success, but not influence that goes the distance.
+                            We bring that perspective to every brand story we
+                            help tell.
+                        </p>
+                    </div>
+                </div>
+            </div>
 
-export default Agence
+            <div className="section2 h-screen">
+                <div className="h-[20vw] w-[15vw] absolute top-72 left-[30vw] rounded-[20px] overflow-hidden">
+                    <img
+                        className="h-full w-full object-cover"
+                        src="./images/agency1.jpg"
+                    ></img>
+                </div>
+                <div className="font-[font2] relative">
+                    <div className="mt-[55vh]">
+                        <h1 className="text-[19vw] uppercase leading-[17vw] text-center">
+                            SEVEN7Y <br></br> TWO
+                        </h1>
+                    </div>
+                    <div className="pl-[40%] font-[font1]">
+                        <p className="text-6xl ">
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            We’re inquisitive and open-minded, and we make sure
+                            creativity crowds out ego from every corner. A brand
+                            is a living thing, with values, a personality and a
+                            story. If we ignore that, we can achieve short-term
+                            success, but not influence that goes the distance.
+                            We bring that perspective to every brand story we
+                            help tell.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Agence;
